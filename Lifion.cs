d060d19e1ad3dd9bd86bc8b5b9ba21305b0fb26c
adjit <atawil@METRO.corp>
2018-04-19 10:20:06 -0400

    Initial function build
    Function build

---
+++ Lifion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Class1
+{
+    public class Payment
+    {
+        DateTime PaymentDate;
+        int PaymentAmount;
+    }
+
+    public class Accounts_Receivable
+    {
+        int Accounts_Receivable_ID;
+        int Amount_Under_30;
+        int Amount_Over_30;
+        List<Payment> Payments;
+    }
+
+    public class Subscribers
+    {
+        string Subscriber;
+        int Subscriber_ID;
+        Accounts_Receivable AR;
+        /*  Contacts, address's etc.
+         * 
+         */
+
+        public Subscribers() { }
+    }
+
+	public Class1()
+	{
+
+
+        DateTime now = DateTime.Now.Date;
+
+
+
+        if(invoices[i].invoiceDate < )
+	}
+}
